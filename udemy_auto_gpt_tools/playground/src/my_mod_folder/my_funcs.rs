9128cd128a7a8ddce4cf28902594eaa4d6f5e733
Mats Wikmar <matwik@gmail.com>
2024-02-11 16:37:54 +0100

    added playgroud docblock html

--- udemy_auto_gpt_tools/playground/src/my_mod_folder/my_funcs.rs
+++ udemy_auto_gpt_tools/playground/src/my_mod_folder/my_funcs.rs
@@ -1,5 +1,11 @@
 
-
+/// Function: add_ten
+/// 
+/// # Example
+/// ```
+/// let x: u32 = 5;
+/// let y: u32 = add_ten(x); // mjao
+/// ```
 pub fn add_ten(num: u32) -> u32 {
     num + 10
 }
@@ -19,6 +25,11 @@ mod test {
 
             RUST_BACKTRACE=1 cargo test     // run test with a stacktrace 
                 - NOTE: $env:RUST_BACKTRACE=1 ..if using pwsh
+        */
+
+        /*
+            cargo doc                       // generate a /doc/.. in target-DIR
+            cargo doc --open                // open html with the docs
         */
 
 
